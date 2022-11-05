fcfac6f0aa66b660ccc8d3112e22c55551055319
Hitray <75577433+Hitray@users.noreply.github.com>
2022-11-05 08:32:14 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
+11 lines (9 sloc)  383 Bytes
 
-        SearchServer searchServer = new SearchServer(8989);
-        searchServer.start();
+public class Main {
+    public static final int PORT = 8989;
+
+    public static void main(String[] args) throws Exception {
+        SearchServer server = new SearchServer(PORT);
+        server.start();
     }
 }
