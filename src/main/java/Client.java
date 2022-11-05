c3da49e5d4d00669ca3d3aef837b37736eb38c10
Hitray <75577433+Hitray@users.noreply.github.com>
2022-11-05 11:57:11 +0500

    Update Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,21 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import com.google.gson.*;
+import java.io.*;
 import java.net.Socket;
-
+import java.util.Scanner;
 
 public class Client {
-    private static final int PORT = 8989;
-    private static final String SERVER_IP = "localhost";
-    public static void main(String[] args) throws IOException {
-        try (
-                Socket socket = new Socket(SERVER_IP, PORT);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
-        ) {
-            out.println("контроля DevOps");
-            System.out.println(in.readLine());
+    public static void main(String[] args) {
+        String HOST = "localhost";
+        int PORT = 8989;
+        Scanner scanner = new Scanner(System.in);
+
+        try (Socket clientSocket = new Socket(HOST, PORT);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            String questionFromServer = in.readLine();
+            System.out.println(questionFromServer);
+            String answerToServer = scanner.nextLine();
+            out.println(answerToServer);
+            String uglyJsonFromServer = in.readLine();
+            System.out.println(getPrettyJson(uglyJsonFromServer));
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
+    }
+
+    public static String getPrettyJson(String uglyJson) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(uglyJson);
+        return gson.toJson(jsonElement);
     }
 }
