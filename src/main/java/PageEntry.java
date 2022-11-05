5d1fa49c38c4683bb72599dc03058d3ce308ed86
Hitray <75577433+Hitray@users.noreply.github.com>
2022-11-05 08:30:21 +0500

    Update PageEntry.java

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,12 +1,7 @@
-import java.util.Objects;
-
 public class PageEntry implements Comparable<PageEntry> {
-    private String pdfName;
-    private int page;
-    private int count;
-
-    public PageEntry() {
-    }
+    private final String pdfName;
+    private final int page;
+    private final int count;
 
     public PageEntry(String pdfName, int page, int count) {
         this.pdfName = pdfName;
@@ -25,31 +20,6 @@ public class PageEntry implements Comparable<PageEntry> {
     public int getCount() {
         return count;
     }
-
-
-    @Override
-    public String toString() {
-        return "\nPageEntry {\n " +
-                "pdfName = '" + pdfName + '\'' +
-                ",\n page = " + page +
-                ",\n count = " + count +
-                "\n}";
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PageEntry)) return false;
-        PageEntry pageEntry = (PageEntry) o;
-        return getPage() == pageEntry.getPage() && getCount() == pageEntry.getCount() && Objects.equals(getPdfName(), pageEntry.getPdfName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getPdfName(), getPage(), getCount());
-    }
-
 
     @Override
     public int compareTo(PageEntry o) {
