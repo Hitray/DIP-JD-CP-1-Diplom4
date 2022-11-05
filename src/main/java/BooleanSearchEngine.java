19ede06cf1e0b013d055054f9eb9e5b867af6f08
Hitray <75577433+Hitray@users.noreply.github.com>
2022-11-05 08:37:41 +0500

    Update BooleanSearchEngine.java

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -4,77 +4,81 @@ import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class BooleanSearchEngine implements SearchEngine {
-    private final Map<String, List<PageEntry>> words;
-    private static final List<String> stopWords;
 
-    static {
-        try {
-            stopWords = Files.readAllLines(Paths.get("stop-ru.txt"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private Map<String, List<PageEntry>> words;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        List<File> PDFList = List.of(Objects.requireNonNull(pdfsDir.listFiles()));
-        words = new HashMap<>();
-        for (File pdf : PDFList) {
-            var doc = new PdfDocument(new PdfReader(pdf));
-            for (int i = 0; i < doc.getNumberOfPages(); i++) {
-                var text = PdfTextExtractor.getTextFromPage(doc.getPage(i + 1));
-                var words = Stream.of(text.toLowerCase().split("\\P{IsAlphabetic}+"))
-                        .collect(Collectors.toCollection(ArrayList<String>::new));
-                words.removeAll(stopWords);
 
-                Map<String, Integer> freqs = new HashMap<>();
-                for (var word : words) {
+        List<File> listOfPDFFiles = List.of(Objects.requireNonNull(pdfsDir.listFiles())); 
+
+        words = new HashMap<>();
+
+        for (File pdf : listOfPDFFiles) { 
+
+            var doc = new PdfDocument(new PdfReader(pdf)); 
+            for (int i = 0; i < doc.getNumberOfPages(); i++) { 
+
+                var textOfOnePage = PdfTextExtractor.getTextFromPage(doc.getPage(i + 1)); 
+
+                var allWordsOnPage = textOfOnePage.split("\\P{IsAlphabetic}+"); 
+
+                Map<String, Integer> freqs = new HashMap<>(); 
+
+                
+                for (var word : allWordsOnPage) {
                     if (word.isEmpty()) {
                         continue;
                     }
                     freqs.put(word.toLowerCase(), freqs.getOrDefault(word.toLowerCase(), 0) + 1);
                 }
-                int count;
+                
+                int count = 0;
                 for (var word : freqs.keySet()) {
-                    if (freqs.get(word.toLowerCase()) != null) {
-                        count = freqs.get(word.toLowerCase());
-                        this.words.computeIfAbsent(word.toLowerCase(), w -> new ArrayList<>()).add(new PageEntry(pdf.getName(), i + 1, count));
+                    String wordToLowerCase = word.toLowerCase();
+                    if (freqs.get(wordToLowerCase) != null) {
+                        count = freqs.get(wordToLowerCase);
+                        words.computeIfAbsent(wordToLowerCase, k -> new ArrayList<>()).add(new PageEntry(pdf.getName(), i + 1, count));
                     }
                 }
-                freqs.clear();
+                freqs.clear(); 
             }
         }
-
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        List<PageEntry> tempList = new ArrayList<>();
-        List<PageEntry> result = new ArrayList<>();
-        String[] request = word.split("\\P{IsAlphabetic}+");
-        for (String newRequest : request) {
-            if (words.get(newRequest.toLowerCase()) != null) {
-                tempList.addAll(words.get(newRequest.toLowerCase()));
+       
+        List<PageEntry> result = new ArrayList<>(); 
+        String wordToLowerCase = word.toLowerCase(); 
+        if (words.get(wordToLowerCase) != null) { 
+            for (PageEntry pageEntry : words.get(wordToLowerCase)) { 
+                result.add(pageEntry); 
             }
         }
-        Map<String, Map<Integer, Integer>> pageNumberAndCounter = new HashMap<>();
-        for (PageEntry pageEntry : tempList) {
-            pageNumberAndCounter.computeIfAbsent(pageEntry.getPdfName(), key -> new HashMap<>()).merge(pageEntry.getPage(), pageEntry.getCount(), Integer::sum);
-        }
-
-        pageNumberAndCounter.forEach((key, value) -> {
-            for (var tempPage : value.entrySet()) {
-                result.add(new PageEntry(key, tempPage.getKey(), tempPage.getValue()));
-            }
-        });
-        Collections.sort(result);
+        Collections.sort(result); 
         return result;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BooleanSearchEngine)) return false;
+        BooleanSearchEngine that = (BooleanSearchEngine) o;
+        return Objects.equals(words, that.words);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(words);
+    }
+
+    @Override
+    public String toString() {
+        return "BooleanSearchEngine{" +
+                "words=" + words +
+                '}';
     }
 }
